3c1166e81385a8e629730764874820bbc63724fe
Don-Stevenson <dsteven1@gmail.com>
2020-04-09 00:30:08 -0400

    returning todo list as console.log

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import Todos from './components/Todos'
+import React, {Component} from "react";
+import Todos from "./components/Todos";
 
-
-
-function App() {
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, title: "take out the trash", completed: false },
+      { id: 2, title: "dinner with gf", completed: false },
+      { id: 3, title: "meeting with boss", completed: false }
+    ]
+  };
+  
+  render () {
+    console.log(this.state.todos.title)
   return (
     <div className="App">
-      <Todos />
+      <Todos todos={this.state.todos} />
     </div>
-  );
+  ); 
+ }
 }
 
 export default App;
