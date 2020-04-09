8899f9c00f29969ee5f6d7f4896dd3251ab2cc3b
Don-Stevenson <dsteven1@gmail.com>
2020-04-09 01:10:30 -0400

    set up markcomplete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Todos from "./components/Todos";
 
 class App extends Component {
@@ -10,18 +10,25 @@ class App extends Component {
     ]
   };
 
-  markComplete = () => {
-    console.log("from app js")
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  render() {
+    console.log(this.state.todos.title);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
   }
-  
-  render () {
-    console.log(this.state.todos.title)
-  return (
-    <div className="App">
-      <Todos todos={this.state.todos} markComplete={this.markComplete}/>
-    </div>
-  ); 
- }
 }
 
 export default App;
