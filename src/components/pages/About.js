49b8aa498bb124a2780db98c60dffad80fa34409
Don-Stevenson <dsteven1@gmail.com>
2020-04-10 18:58:40 -0400

    update about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <React.Fragment>
       <h1>About</h1>
-      <p>This is the TodoList app v1.0.0. It is part of a React crash course</p>
+      <p>This is a TodoList app v1.0.0. Built as part of a React crash course: Apr. 8 - 10, 2020</p>
     </React.Fragment>
   )
 }
