9def6dfbe7a00a20ba0be74bd1df5009375edf02
Don-Stevenson <dsteven1@gmail.com>
2020-04-09 23:07:10 -0400

    working delete bug fixed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react'
 
-export default function About() {
+function About() {
   return (
     <React.Fragment>
       <h1>About</h1>
-      <p>This is the TodoList app v1.0.0. It is part of a React tutorial</p>
+      <p>This is the TodoList app v1.0.0. It is part of a React crash course</p>
     </React.Fragment>
-  );
+  )
 }
+
+
+export default About;
